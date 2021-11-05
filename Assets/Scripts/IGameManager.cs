365eaeba6f6e5529ee69453245fa9b35edf5acf2
Dorrekens <thomas.dorrekens@student.vinci.be>
2021-11-05 02:32:58 +0100

    trap and checkpoint

--- Assets/Scripts/IGameManager.cs
+++ Assets/Scripts/IGameManager.cs
@@ -1,10 +1,12 @@
 using System;
+using UnityEngine;
 
 public interface IGameManager
 {
     void Quit();
     void StartNewGame();
     void SwitchScene(string sceneToLoad);
-
     void EndGame();
+    void NewCheckpoint(GameObject checkpoint);
+    void RestartToCheckpoint(GameObject player);
 }
